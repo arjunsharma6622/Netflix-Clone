1d3fac6c6e2eb131531dee2c97355af897751c07
Arjun Sharma <arjunsharma6622@gmail.com>
2023-08-22 08:43:00 +0530

    user schema update

--- api/models/User.js
+++ api/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 
 const UserSchema = new mongoose.Schema({
-    username : {type: String, unique: true},
+    username : {type: String},
     email : {type: String, required: true, unique: true},
     password : {type: String, required: true},
     profilePic : {type: String, default: ""},
