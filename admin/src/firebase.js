120e5893bc0a77ae7f8180f972b1a9120a21f0f9
Arjun Sharma <arjunsharma6622@gmail.com>
2023-08-26 23:53:45 +0530

    new product layout done and firebase started

---
+++ admin/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCSkCc2b0h4n1C4ILkrhof71SSETh-2D-A",
+    authDomain: "netflix-clone-6622.firebaseapp.com",
+    projectId: "netflix-clone-6622",
+    storageBucket: "netflix-clone-6622.appspot.com",
+    messagingSenderId: "788212843494",
+    appId: "1:788212843494:web:40a32cbdbcdfef1a4b4c27",
+    measurementId: "G-XV7RPKDBKN"
+  };
+
+
+firebase.initializeApp(firebaseConfig)
+const storage = firebase.storage()
+export default storage
--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -1,30 +1,97 @@
+import { useState } from "react";
 import "./newProduct.css";
 
 export default function NewProduct() {
+
+  const [movie, setMovie] = useState(null)
+  const [image, setImage] = useState(null)
+  const [imageTitle, setImageTitle] = useState(null)
+  const [imageSm, setImageSm] = useState(null)
+  const [trailer, setTrailer] = useState(null)
+  const [video, setVideo] = useState(null)
+  const [uploaded, setUploaded] = useState(0)
+
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setMovie({ ...movie, [e.target.name]: value })
+  }
+
+  console.log(movie)
+
+
+
   return (
     <div className="newProduct">
-      <h1 className="addProductTitle">New Product</h1>
+      <h1 className="addProductTitle">New Movie</h1>
       <form className="addProductForm">
+
+
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" />
+          <input type="file" id="img" name="img" onChange={(e) => setImage(e.target.files[0])}/>
         </div>
         <div className="addProductItem">
-          <label>Name</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <label>Title Image</label>
+          <input type="file" id="imgTitle" name="imgTitle" onChange={(e) => setImageTitle(e.target.files[0])}/>
         </div>
         <div className="addProductItem">
-          <label>Stock</label>
-          <input type="text" placeholder="123" />
+          <label>Thumbnail Image</label>
+          <input type="file" id="imgSm" name="imgSm" onChange={(e) => setImageSm(e.target.files[0])}/>
+        </div>
+
+
+
+        <div className="addProductItem">
+          <label>Title</label>
+          <input type="text" placeholder="John Wick" name="title" onChange={handleChange} />
         </div>
         <div className="addProductItem">
-          <label>Active</label>
-          <select name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
+          <label>Description</label>
+          <input type="text" placeholder="Description" name="desc" onChange={handleChange} />
+        </div>
+        <div className="addProductItem">
+          <label>Year</label>
+          <input type="text" placeholder="Year" name="year" onChange={handleChange} />
+        </div>
+        <div className="addProductItem">
+          <label>Genre</label>
+          <input type="text" placeholder="Genre" name="genre" onChange={handleChange} />
+        </div>
+        <div className="addProductItem">
+          <label>Duration</label>
+          <input type="text" placeholder="Duration" name="duration" onChange={handleChange} />
+        </div>
+        <div className="addProductItem">
+          <label>Limit</label>
+          <input type="text" placeholder="Limit" name="limit" onChange={handleChange} />
+        </div>
+
+
+        <div className="addProductItem">
+          <label>Is Series?</label>
+          <select name="isSeries" id="isSeries" onChange={handleChange}>
+            <option value="false">No</option>
+            <option value="true">Yes</option>
           </select>
         </div>
-        <button className="addProductButton">Create</button>
+
+
+        <div className="addProductItem">
+          <label>Trailer</label>
+          <input type="file" name="trailer" onChange={(e) => setTrailer(e.target.files[0])}/>
+        </div>
+        <div className="addProductItem">
+          <label>Video</label>
+          <input type="file" name="video" onChange={(e) => setVideo(e.target.files[0])}/>
+        </div>
+
+        {uploaded === 5 ? (
+          <button className="addProductButton">Create</button>
+        ) : (
+          <button className="addProductButton">Upload</button>
+        )}
+        
       </form>
     </div>
   );
