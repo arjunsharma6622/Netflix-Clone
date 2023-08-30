d2658c46aa593a74671904c0afdc4148354176cb
Arjun Sharma <arjunsharma6622@gmail.com>
2023-08-31 00:08:49 +0530

    lists context api calls started

--- admin/src/context/listContext/apiCalls.js
+++ admin/src/context/listContext/apiCalls.js
@@ -1,20 +1,20 @@
-import { createMoviesFailure, createMoviesStart, createMoviesSuccess, deleteMovieFailure, deleteMovieStart, deleteMovieSuccess, getMoviesFailure, getMoviesStart, getMoviesSuccess, updateMoviesFailure, updateMoviesStart, updateMoviesSuccess } from "./MovieActions"
+import { getListsFailure, getListsStart, getListsSuccess } from "./ListActions"
 import axios from "axios"
 
-export const getMovies = async (dispatch) => {
-    dispatch(getMoviesStart())
+export const getLists = async (dispatch) => {
+    dispatch(getListsStart())
 
     try{
-        const res = await axios.get("http://localhost:8000/api/movie/", {
+        const res = await axios.get("http://localhost:8000/api/lists/", {
             headers: {
                 "token": "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
             }
         })
 
-        dispatch(getMoviesSuccess(res.data))
+        dispatch(getListsSuccess(res.data))
     }
     catch(err){
-        dispatch(getMoviesFailure())
+        dispatch(getListsFailure())
         console.log(err)
     }
 }
@@ -22,52 +22,52 @@ export const getMovies = async (dispatch) => {
 
 
 
-export const createMovie = async (movie, dispatch) => {
-    dispatch(createMoviesStart())
+// export const createMovie = async (movie, dispatch) => {
+//     dispatch(createMoviesStart())
 
-    try{
-        const res = await axios.post(`http://localhost:8000/api/movie/`, movie , {
-            headers: {
-                "token": "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
-            }
-        })
-        dispatch(createMoviesSuccess(res.data))
-    }
-    catch(err){
-        dispatch(createMoviesFailure())
-    }
-}
-
-
-export const updateMovie = async (movie, dispatch) => {
-    dispatch(updateMoviesStart())
-
-    try{
-        const res = await axios.put(`http://localhost:8000/api/movie/${movie._id}`, movie, {
-            headers: {
-                "token" : "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
-            }
-        })
-        dispatch(updateMoviesSuccess(res.data))
-    }
-    catch(err){
-        dispatch(updateMoviesFailure())
-    }
-}
+//     try{
+//         const res = await axios.post(`http://localhost:8000/api/movie/`, movie , {
+//             headers: {
+//                 "token": "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
+//             }
+//         })
+//         dispatch(createMoviesSuccess(res.data))
+//     }
+//     catch(err){
+//         dispatch(createMoviesFailure())
+//     }
+// }
 
 
-export const deleteMovie = async (id, dispatch) => {
-    dispatch(deleteMovieStart())
+// export const updateMovie = async (movie, dispatch) => {
+//     dispatch(updateMoviesStart())
 
-    try{
-        await axios.delete(`http://localhost:8000/api/movie/${id}`, {
-            headers: {
-                "token": "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
-            }
-        })
-        dispatch(deleteMovieSuccess(id))
-    }
-    catch(err){
-        dispatch(deleteMovieFailure())
-    }
-}
+//     try{
+//         const res = await axios.put(`http://localhost:8000/api/movie/${movie._id}`, movie, {
+//             headers: {
+//                 "token" : "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
+//             }
+//         })
+//         dispatch(updateMoviesSuccess(res.data))
+//     }
+//     catch(err){
+//         dispatch(updateMoviesFailure())
+//     }
+// }
+
+
+// export const deleteMovie = async (id, dispatch) => {
+//     dispatch(deleteMovieStart())
+
+//     try{
+//         await axios.delete(`http://localhost:8000/api/movie/${id}`, {
+//             headers: {
+//                 "token": "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
+//             }
+//         })
+//         dispatch(deleteMovieSuccess(id))
+//     }
+//     catch(err){
+//         dispatch(deleteMovieFailure())
+//     }
+// }
