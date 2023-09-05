903debaed0f5d228f147ff14564acb1079bb845a
Arjun Sharma <arjunsharma6622@gmail.com>
2023-09-06 00:40:29 +0530

    create list page is done, context api yet to be created

--- admin/src/pages/newList/NewList.jsx
+++ admin/src/pages/newList/NewList.jsx
@@ -1,10 +1,10 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./NewList.css";
 // import storage from "../../fbase";
 import app from "../../firebase"; // Import as default
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { createMovie } from "../../context/movieContext/apiCalls";
-import {MovieContext} from '../../context/movieContext/MovieContext'
+import { createMovie, getMovies } from "../../context/movieContext/apiCalls";
+import { MovieContext } from '../../context/movieContext/MovieContext'
 import { ListContext } from "../../context/listContext/ListContext";
 const storage = getStorage(app);
 
@@ -14,16 +14,31 @@ const storage = getStorage(app);
 
 export default function NewList() {
 
+
   const [list, setList] = useState(null)
 
 
-  const {dispatch} = useContext(ListContext)
-  const {movies, dispatch : dispatchMovie} = useContext(MovieContext)
+  const { dispatch } = useContext(ListContext)
+  const { movies, dispatch: dispatchMovie } = useContext(MovieContext)
+
+
+
+  useEffect(() => {
+    getMovies(dispatchMovie)
+  }, [dispatchMovie])
+
 
   const handleChange = (e) => {
     const value = e.target.value;
     setList({ ...list, [e.target.name]: value })
   }
+
+  const handleSelect = (e) => {
+    let value = Array.from(e.target.selectedOptions, option => option.value)
+    setList({ ...list, [e.target.name]: value })
+  }
+
+  console.log(list)
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -34,23 +49,43 @@ export default function NewList() {
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Movie</h1>
-      <form className="addProductForm">
+      <form className="addProductForm" style={{display: "flex", alignItems: "start"}}>
+
+        <div className="formLeft">
 
 
-        <div className="addProductItem">
-          <label>Title</label>
-          <input type="text" placeholder="John Wick" name="title" onChange={handleChange} />
+          <div className="addProductItem">
+            <label>Title</label>
+            <input type="text" placeholder="John Wick" name="title" onChange={handleChange} />
+          </div>
+
+          <div className="addProductItem">
+            <label>Genre</label>
+            <input type="text" placeholder="Genre" name="genre" onChange={handleChange} />
+          </div>
+
+          <div className="addProductItem">
+            <label>Type</label>
+            <select name="type" id="" onChange={handleChange}>
+              <option>Type</option>
+              <option value="movie">Movie</option>
+              <option value="series">Series</option>
+            </select>
+          </div>
+
+
         </div>
-        <div className="addProductItem">
-          <label>Genre</label>
-          <input type="text" placeholder="Description" name="genre" onChange={handleChange} />
-        </div>
-        <div className="addProductItem">
-          <label>Type</label>
-          <select name="type" id="" onChange={handleChange}>
-            <option value="movie">Movie</option>
-            <option value="series">Series</option>
-          </select>
+
+
+        <div className="formRight">
+          <div className="addProductItem">
+            <label>Content / Movies</label>
+            <select multiple name="content" id="" onChange={handleSelect} style={{ height: "200px" }}>
+              {movies.map((movie) => (
+                <option key={movie._id} value={movie._id}>{movie.title}</option>
+              ))}
+            </select>
+          </div>
         </div>
         <button className="addProductButton" onClick={handleSubmit}>Create</button>
       </form>
